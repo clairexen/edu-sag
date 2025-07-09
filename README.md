d8274b1610cafd93f36462f225d04990dd814296
Claire Xenia Wolf <claire@clairexen.net>
2025-07-09 19:44:38 +0200

    Add mermaid diagram
    Signed-off-by: Claire Xenia Wolf <claire@clairexen.net>

--- README.md
+++ README.md
@@ -18,3 +18,168 @@ the higher bits of the result in reversed order.
 
 This creates a word where `di` is filtered and reorganized based on `ci`, with
 a forward-ordered prefix of selected bits and a mirrored suffix of the rest.
+
+```mermaid
+block-beta
+  columns 9
+  classDef sheep fill:#faa
+  classDef label fill:#fff,stroke-width:0px
+  block:A:8
+    columns 8
+    A7["s"]
+    A6["t"]
+    A5["C"]
+    A4["A"]
+    A3["a"]
+    A2["T"]
+    A1["o"]
+    A0["g"]
+  end
+space:9 S1["1st Stage"]
+  block:B:8
+    columns 8
+    B7["s"]
+    B6["t"]
+    B5["A"]
+    B4["C"]
+    B3["a"]
+    B2["T"]
+    B1["g"]
+    B0["o"]
+  end
+space:9 U1["Unshuffle"]
+  block:C:8
+    columns 8
+    C7["s"]
+    C6["A"]
+    C5["a"]
+    C4["g"]
+    C3["t"]
+    C2["C"]
+    C1["T"]
+    C0["o"]
+  end
+space:9 S2["2nd Stage"]
+  block:D:8
+    columns 8
+    D7["s"]
+    D6["A"]
+    D5["g"]
+    D4["a"]
+    D3["C"]
+    D2["t"]
+    D1["o"]
+    D0["T"]
+  end
+space:9 U2["Unshuffle"]
+  block:E:8
+    columns 8
+    E7["s"]
+    E6["g"]
+    E5["C"]
+    E4["o"]
+    E3["A"]
+    E2["a"]
+    E1["t"]
+    E0["T"]
+  end
+space:9 S3["3rd Stage"]
+  block:F:8
+    columns 8
+    F7["g"]
+    F6["s"]
+    F5["o"]
+    F4["C"]
+    F3["a"]
+    F2["A"]
+    F1["t"]
+    F0["T"]
+  end
+space:9 U3["Unshuffle"]
+  block:G:8
+    columns 8
+    G7["g"]
+    G6["o"]
+    G5["a"]
+    G4["t"]
+    G3["s"]
+    G2["C"]
+    G1["A"]
+    G0["T"]
+  end
+class S1 label
+class U1 label
+class S2 label
+class U2 label
+class S3 label
+class U3 label
+class A2 sheep
+class A4 sheep
+class A5 sheep
+class B2 sheep
+class B4 sheep
+class B5 sheep
+class C1 sheep
+class C2 sheep
+class C6 sheep
+class D0 sheep
+class D3 sheep
+class D6 sheep
+class E0 sheep
+class E3 sheep
+class E5 sheep
+class F0 sheep
+class F2 sheep
+class F4 sheep
+class G0 sheep
+class G1 sheep
+class G2 sheep
+A0 --> B1
+A1 --> B0
+A2 --> B2
+A3 --> B3
+A4 --> B5
+A5 --> B4
+A6 --> B6
+A7 --> B7
+C0 --> D1
+C1 --> D0
+C2 --> D3
+C3 --> D2
+C4 --> D5
+C5 --> D4
+C6 --> D6
+C7 --> D7
+E0 --> F0
+E1 --> F1
+E2 --> F3
+E3 --> F2
+E4 --> F5
+E5 --> F4
+E6 --> F7
+E7 --> F6
+B0 --> C0
+B2 --> C1
+B4 --> C2
+B6 --> C3
+B1 --> C4
+B3 --> C5
+B5 --> C6
+B7 --> C7
+D0 --> E0
+D2 --> E1
+D4 --> E2
+D6 --> E3
+D1 --> E4
+D3 --> E5
+D5 --> E6
+D7 --> E7
+F0 --> G0
+F2 --> G1
+F4 --> G2
+F6 --> G3
+F1 --> G4
+F3 --> G5
+F5 --> G6
+F7 --> G7
+```
---
+++ mermaid.py
@@ -0,0 +1,193 @@
+#  Educational 8-Bit Sheep-And-Goats (SAG) Verilog Reference IP
+#
+#  Copyright (C) 2025  Claire Xenia Wolf <claire@clairexen.net>
+#
+#  Permission to use, copy, modify, and/or distribute this software for any
+#  purpose with or without fee is hereby granted, provided that the above
+#  copyright notice and this permission notice appear in all copies.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+#  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+#  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+#  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+#  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+#  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+#  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+class sag:
+    def __init__(self, subject):
+        assert len(subject) == 8
+        self.di = list(reversed(subject))
+        self.ci = [x.isupper() for x in self.di]
+
+        self.c1, self.b1 = self.sagCtrlUnit(self.ci, 0b00) # ctrl_1
+        self.c2, self.b2 = self.sagCtrlUnit(self.c1, 0b10) # ctrl_2
+        self.co, self.b3 = self.sagCtrlUnit(self.c2, 0b11) # ctrl_3
+
+        self.d1 = self.sagDataUnit(self.di, self.b1) # data_1
+        self.d2 = self.sagDataUnit(self.d1, self.b2) # data_2
+        self.do = self.sagDataUnit(self.d2, self.b3) # data_3
+
+    def sagCtrlUnit(self, ci, sel):
+        x = list() # prefix xor-sum over ci, partially broken depending on sel
+        x.append(ci[0])
+        x.append(ci[1] != x[0])
+        x.append(ci[2] != (False if sel&1 else x[1]))
+        x.append(ci[3] != x[2])
+        x.append(ci[4] != (False if sel&2 else x[3]))
+        x.append(ci[5] != x[4])
+        x.append(ci[6] != (False if sel&1 else x[5]))
+        x.append(ci[7] != x[6]) # unused ;)
+
+        # butterfly control signals
+        b = list()
+        b.append(not x[0])
+        b.append(not x[2])
+        b.append(not x[4])
+        b.append(not x[6])
+
+        co = self.sagDataUnit(ci, b) # ctrl_bfly
+        return (co, b)
+
+    def sagDataUnit(self, di, b):
+        do_shuffled = list()
+        do_shuffled += [di[1], di[0]] if b[0] else di[0:2]
+        do_shuffled += [di[3], di[2]] if b[1] else di[2:4]
+        do_shuffled += [di[5], di[4]] if b[2] else di[4:6]
+        do_shuffled += [di[7], di[6]] if b[3] else di[6:8]
+        do = self.sagUnshuffle(do_shuffled)
+        return do
+
+    def sagUnshuffle(self, di):
+        even = [di[0], di[2], di[4], di[6]]
+        odds = [di[1], di[3], di[5], di[7]]
+        do = even + odds
+        return do
+
+def shuffle(a):
+    return [a[0], a[4], a[1], a[5], a[2], a[6], a[3], a[7]]
+    
+subject="stCAaTog"
+d = sag(subject)
+
+def jrs(a):
+    return " ".join(reversed(shuffle(a)))
+
+def jru(a):
+    return " ".join(reversed(a))
+
+def st(s, n):
+    def tag(i):
+        nonlocal s
+        s = s[:i] + "|" + s[i+1:]
+    if n & 2: tag(3)
+    if n & 1: tag(7)
+    if n & 2: tag(11)
+    return s
+
+# print(f"di=[{st(jru(d.di), 0)}] ci=[{st(jru(['1' if x else '0' for x in d.ci]), 0)}]")
+# print(f"   [{st(jrs(d.d1), 0)}]    [{st(jrs(['1' if x else '0' for x in d.c1]), 0)}]")
+# print(f"d1=[{st(jru(d.d1), 1)}] c1=[{st(jru(['1' if x else '0' for x in d.c1]), 1)}]")
+# print(f"   [{st(jrs(d.d2), 1)}]    [{st(jrs(['1' if x else '0' for x in d.c2]), 1)}]")
+# print(f"d2=[{st(jru(d.d2), 3)}] c2=[{st(jru(['1' if x else '0' for x in d.c2]), 3)}]")
+# print(f"   [{st(jrs(d.do), 3)}]    [{st(jrs(['1' if x else '0' for x in d.co]), 3)}]")
+# print(f"do=[{st(jru(d.do), 0)}] co=[{st(jru(['1' if x else '0' for x in d.co]), 0)}]")
+
+A = d.di
+B = shuffle(d.d1)
+C = d.d1
+D = shuffle(d.d2)
+E = d.d2
+F = shuffle(d.do)
+G = d.do
+
+print(f"block-beta")
+print(f"  columns 9")
+print(f"  classDef sheep fill:#faa")
+print(f"  classDef label fill:#fff,stroke-width:0px")
+
+def mm_block(letter, chars):
+    print(f"  block:{letter}:8")
+    print(f"    columns 8")
+    for i in range(7, -1, -1):
+        print(f"    {letter}{i}[\"{chars[i]}\"]")
+    print(f"  end")
+
+mm_block("A", A)
+print("space:9 S1[\"1st Stage\"]")
+mm_block("B", B)
+print("space:9 U1[\"Unshuffle\"]")
+mm_block("C", C)
+print("space:9 S2[\"2nd Stage\"]")
+mm_block("D", D)
+print("space:9 U2[\"Unshuffle\"]")
+mm_block("E", E)
+print("space:9 S3[\"3rd Stage\"]")
+mm_block("F", F)
+print("space:9 U3[\"Unshuffle\"]")
+mm_block("G", G)
+
+for i in range(1, 4):
+    print(f"class S{i} label")
+    print(f"class U{i} label")
+
+def mm_sheep(letter, chars):
+    for i in range (8):
+        if chars[i].isupper():
+            print(f"class {letter}{i} sheep")
+
+mm_sheep("A", A)
+mm_sheep("B", B)
+mm_sheep("C", C)
+mm_sheep("D", D)
+mm_sheep("E", E)
+mm_sheep("F", F)
+mm_sheep("G", G)
+
+def mm_swap(a, b, c):
+    if c[0]:
+        print(f"{a}0 --> {b}1")
+        print(f"{a}1 --> {b}0")
+    else:
+        print(f"{a}0 --> {b}0")
+        print(f"{a}1 --> {b}1")
+
+    if c[1]:
+        print(f"{a}2 --> {b}3")
+        print(f"{a}3 --> {b}2")
+    else:
+        print(f"{a}2 --> {b}2")
+        print(f"{a}3 --> {b}3")
+
+    if c[2]:
+        print(f"{a}4 --> {b}5")
+        print(f"{a}5 --> {b}4")
+    else:
+        print(f"{a}4 --> {b}4")
+        print(f"{a}5 --> {b}5")
+
+    if c[3]:
+        print(f"{a}6 --> {b}7")
+        print(f"{a}7 --> {b}6")
+    else:
+        print(f"{a}6 --> {b}6")
+        print(f"{a}7 --> {b}7")
+
+mm_swap("A", "B", d.b1)
+mm_swap("C", "D", d.b2)
+mm_swap("E", "F", d.b3)
+
+def mm_shuffle(a, b):
+    print(f"{a}0 --> {b}0")
+    print(f"{a}2 --> {b}1")
+    print(f"{a}4 --> {b}2")
+    print(f"{a}6 --> {b}3")
+    print(f"{a}1 --> {b}4")
+    print(f"{a}3 --> {b}5")
+    print(f"{a}5 --> {b}6")
+    print(f"{a}7 --> {b}7")
+
+mm_shuffle("B", "C")
+mm_shuffle("D", "E")
+mm_shuffle("F", "G")
+
